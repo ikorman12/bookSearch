01d73d2faa8374cdd97e83da52929accb741a5a2
Igor Korman <ikorman12@gmail.com>
2022-12-07 20:58:32 -0500

    added middleware and apollo client to server.js at root directory

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -55,6 +55,7 @@ const SearchBooks = () => {
         title: book.volumeInfo.title,
         description: book.volumeInfo.description,
         image: book.volumeInfo.imageLinks?.thumbnail || "",
+        link: book.selfLink
       }));
 
       setSearchedBooks(bookData);
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,63 @@
 import { gql } from "@apollo/client";
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($data: BookInput!) {
+    saveBook(data: $data) {
+      _id
+      username
+      email
+      savedBooks {
+        _id
+        description
+        bookId
+        image
+        link
+        title
+        authors
+      }
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        _id
+        description
+        bookId
+        image
+        link
+        title
+        authors
+      }
+    }
+  }
+`;
