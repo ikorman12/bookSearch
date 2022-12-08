88ec9a4e408c68dba8181f291d03b043405912d0
Igor Korman <ikorman12@gmail.com>
2022-12-07 19:43:57 -0500

    added typeDefs file with query and mutations as per readme

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,47 @@
-const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
+const { gql } = require('apollo-server-express');
 
-module.exports = { typeDefs, resolvers };
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String!
+        password: String!
+        email: String!
+        savedBooks: [Book]!
+    }
+
+    type Book {
+        _id:ID
+        authors: [String]!
+        description: String!
+        title: String!
+        description: String!
+        image: String
+        link: String!
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    input BookInput {
+        descirption: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+       
+        
+    type Query {
+        me: User
+    }
+
+    type mutation {
+        addUser(username:Stting!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        saveBook(data: BookInput):User
+        removeBook(bookId: ID!): User
+    }
+    `;
+
+    module.exports = typeDefs;
